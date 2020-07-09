fe049aff4df48973e74205a6fbb35821ea882fc9
Gextox <gabriel_il@live.com>
2020-07-09 01:10:09 -0300

    Updates

---
+++ precache-manifest.b1fbaf55a0ed31e22d3a68f2fc9a81ef.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "271ae1d214214a87eb2a080976233742",
+    "url": "/br-ba-quotes/index.html"
+  },
+  {
+    "revision": "028b1bac4303429c3b12",
+    "url": "/br-ba-quotes/static/css/main.f4010300.chunk.css"
+  },
+  {
+    "revision": "5dafffc1e77f4ceebe5d",
+    "url": "/br-ba-quotes/static/js/2.e658478b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/br-ba-quotes/static/js/2.e658478b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "028b1bac4303429c3b12",
+    "url": "/br-ba-quotes/static/js/main.0da39709.chunk.js"
+  },
+  {
+    "revision": "081fe1b5145e5fc87ce7",
+    "url": "/br-ba-quotes/static/js/runtime-main.2dc2627e.js"
+  },
+  {
+    "revision": "88e9414e2adcde532c8e7f77a23ec3ec",
+    "url": "/br-ba-quotes/static/media/logo.88e9414e.svg"
+  }
+]);
